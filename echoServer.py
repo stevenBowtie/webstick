17b49190d5fc055206a72695e98a2d5f69bb324d
Steven Bowtie <steven.bowtie@gmail.com>
2019-06-13 23:00:41 -0700

    Fix joystick selector and add echoServer for testing

---
+++ echoServer.py
@@ -0,0 +1,14 @@
+import asyncio
+import websockets
+
+async def incoming_handler(websocket, path):
+    while websocket.open:
+      name = await websocket.recv()
+      print(name)
+      websocket.pong()
+    websocket.close()
+
+start_server = websockets.serve(incoming_handler, 'localhost', 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
